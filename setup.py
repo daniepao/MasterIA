ba78d7a7522b8c8b9f0eea7e38130bd9798507b0
daniepao <daniepao_1920@hotmail.com>
2024-06-04 22:30:42 -0500

    prueba

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name = "paquetecalculo",
+    version = "1.0",
+    description = "operaciones mtematicas",
+    author= "Holger Montalvan",
+    author_email= "webdesignvincent@gmail.com",
+    packages=["calculos","calculos.basicos"]
+)
